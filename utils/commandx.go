c33678b87e189baa90177efad5218a87a0ddfdfa
phachon <phachon@163.com>
2018-01-29 17:11:51 +0800

    fix command

--- utils/commandx.go
+++ utils/commandx.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"io/ioutil"
 	"os/exec"
+	"os"
 )
 
 func NewCommandX() *CommandX {
@@ -50,7 +51,8 @@ func (c *CommandX) syncExec(commandXParams CommandXParams) (err error) {
 	if err != nil {
 		return
 	}
-	cmd := exec.Command("/bin/bash", "./"+fileName)
+	cmd := exec.Command("/bin/bash", fileName)
+
 	err = cmd.Run()
 	return
 }
@@ -61,32 +63,42 @@ func (c *CommandX) asyExec(commandXParams CommandXParams) (err error) {
 	if err != nil {
 		return
 	}
-	cmd := exec.Command("/bin/bash", "./"+fileName)
+	cmd := exec.Command("/bin/bash", fileName)
+
 	err = cmd.Start()
 	return
 }
 
 // 创建临时的 shell 脚本文件
-// path 创建脚本目录
+// path 脚本执行目录
 // content 创建的脚本内容
 // isErrorStop 是否遇到错误停止
 func (c *CommandX) createTmpShellFile(path string, content string, isErrorStop bool) (tmpFile string, err error) {
-	file, err := ioutil.TempFile(path, "tmp")
+
+	ok, err := NewFile().PathIsExists("/tmp/.codepub")
+	if err != nil {
+		return
+	}
+	if ok == false {
+		os.Mkdir("/tmp/.codepub", 0777)
+	}
+	file, err := ioutil.TempFile("/tmp/.codepub", "tmp")
 	if err != nil {
 		return
 	}
 	defer file.Close()
 
-	file.Chmod(777)
-	file.WriteString("#!/bin/bash\r\n")
+	file.Chmod(0777)
+	file.WriteString("#!/bin/bash\n")
 	if isErrorStop {
 		// 遇到错误继续执行
-		file.WriteString("set -e\r\n")
+		file.WriteString("set -e\n")
 	}
-	file.WriteString("cd "+path+" \r\n")
+	file.WriteString("cd "+path+" \n")
 	_, err = file.WriteString(content)
 	if err != nil {
 		return
 	}
+
 	return file.Name(), nil
 }
