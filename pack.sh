85f9a7ee6440b6aadb9da37b212a7a9ec9d37554
phachon <phachon@163.com>
2018-04-04 15:05:52 +0800

    fix pack.sh add log dir

--- pack.sh
+++ pack.sh
@@ -11,27 +11,27 @@ mkdir ${RELEASE}
 # windows amd64
 echo 'Start pack windows amd64...'
 GOOS=windows GOARCH=amd64 go build ./
-tar -czvf "${RELEASE}/bzppx-agent-codepub-windows-amd64.tar.gz" bzppx-agent-codepub.exe config.toml cert/ LICENSE README.md
+tar -czvf "${RELEASE}/bzppx-agent-codepub-windows-amd64.tar.gz" bzppx-agent-codepub.exe config.toml cert/ log/.gitignore LICENSE README.md
 rm -rf bzppx-agent-codepub.exe
 
 echo 'Start pack windows X386...'
 GOOS=windows GOARCH=386 go build ./
-tar -czvf "${RELEASE}/bzppx-agent-codepub-windows-386.tar.gz" bzppx-agent-codepub.exe config.toml cert/ LICENSE README.md
+tar -czvf "${RELEASE}/bzppx-agent-codepub-windows-386.tar.gz" bzppx-agent-codepub.exe config.toml cert/ log/.gitignore LICENSE README.md
 rm -rf bzppx-agent-codepub.exe
 
 echo 'Start pack linux amd64'
 GOOS=linux GOARCH=amd64 go build ./
-tar -czvf "${RELEASE}/bzppx-agent-codepub-linux-amd64.tar.gz" bzppx-agent-codepub config.toml cert/ LICENSE README.md
+tar -czvf "${RELEASE}/bzppx-agent-codepub-linux-amd64.tar.gz" bzppx-agent-codepub config.toml cert/ log/.gitignore LICENSE README.md
 rm -rf bzppx-agent-codepub
 
 echo 'Start pack linux 386'
 GOOS=linux GOARCH=386 go build ./
-tar -czvf "${RELEASE}/bzppx-agent-codepub-linux-386.tar.gz" bzppx-agent-codepub config.toml cert/ LICENSE README.md
+tar -czvf "${RELEASE}/bzppx-agent-codepub-linux-386.tar.gz" bzppx-agent-codepub config.toml cert/ log/.gitignore LICENSE README.md
 rm -rf bzppx-agent-codepub
 
 echo 'Start pack mac amd64'
 GOOS=darwin GOARCH=amd64 go build ./
-tar -czvf "${RELEASE}/bzppx-agent-codepub-mac-amd64.tar.gz" bzppx-agent-codepub config.toml cert/ LICENSE README.md
+tar -czvf "${RELEASE}/bzppx-agent-codepub-mac-amd64.tar.gz" bzppx-agent-codepub config.toml cert/ log/.gitignore LICENSE README.md
 rm -rf bzppx-agent-codepub
 
 echo 'END'
